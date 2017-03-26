f5daad8b4ed7fd135b60d054805ee52f1854766b
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-03-26 05:02:17 +0200

    Update main.m

--- main.m
+++ main.m
@@ -1,7 +1,7 @@
 data = fetch_data('key','GOOG/NYSE_JPM','2013-01-01','2013-12-31');
 
-vol1 = estimate_volatility(data{1},'CC2',30,0);
-vol2 = estimate_volatility(data{1},'CC',30,0);
+vol1 = estimate_volatility(data{1},'CC',30,0);
+vol2 = estimate_volatility(data{1},'RS',30,0);
 
 dates = datenum(data{1}.Date);
 
