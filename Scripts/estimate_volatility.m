08a87f23d561f7f4bcb2653067feaf5851b82b2f
Tommaso Belluzzo <tommaso.belluzzo@gmail.com>
2017-04-02 22:55:49 +0200

    Add files via upload

--- Scripts/estimate_volatility.m
+++ Scripts/estimate_volatility.m
@@ -1,56 +1,106 @@
-% CC = Close To Close
-% CCD = Close To Close Demeaned
-% GK = Garman & Klass (1980)
-% HT = Hodges & Tompkins (2002)
-% P = Parkinson (1980)
-% RS = Rogers & Satchell (1991)
-% T = Traditional
-% YZ = Yang & Zhang (2000)
+% [INPUT]
+% data = A t-by-6 table containing the following time series:
+%         - Date (the dates of the observations)
+%         - Open (the opening prices)
+%         - High (the highest prices)
+%         - Low (the lowest prices)
+%         - Close (the closing prices)
+%         - Return (the log returns)
+% est  = A string representing the historical volatility estimator, its value can be one of the following:
+%         - CC (the traditional Close-to-Close estimator)
+%         - CCD (a variant of the previous estimator that uses demeaned returns)
+%         - GK (the estimator proposed by Garman & Klass, 1980)
+%         - GKYZ (a extension of the previous estimator proposed by Yang & Zhang, 2000)
+%         - HT (the estimator proposed by Hodges & Tompkins, 2002)
+%         - P (the estimator proposed by Parkinson, 1980)
+%         - RS (the estimator proposed by Rogers & Satchell, 1991)
+%         - YZ (the estimator proposed by Yang & Zhang, 2000)
+% bw   = An integer representing he bandwidth (dimension) of each rolling window.
+% cln  = A boolean that indicates whether to remove the NaN values at the beginning the result.
+%
+% [OUTPUT]
+% vol  = A column vector containing the estimated historical volatility.
 
-function vol = estimate_volatility(data,est,bw,cln)
+function vol = estimate_volatility(varargin)
+
+    persistent p;
+
+    if isempty(p)
+        p = inputParser();
+        p.addRequired('data',@(x)validateattributes(x,{'table'},{'nonempty','ncols',6}));
+        p.addRequired('est',@(x)any(validatestring(x,{'CC','CCD','GK','GKYZ','HT','P','RS','YZ'})));
+        p.addRequired('bw',@(x)validateattributes(x,{'numeric'},{'scalar','integer','real','finite','>=',2}));
+        p.addOptional('cln',true,@(x) validateattributes(x,{'logical'},{'scalar'}));
+    end
+
+    p.parse(varargin{:});
     
-    if (~ischar(est))
-        error('Invalid estimator specified.');
+    res = p.Results;
+    data = res.data;
+    est = res.est;
+    bw = res.bw;
+    cln = res.cln;
+
+    if (bw >= size(data,1))
+        error('The number of observations must be greater than the dimension of each rolling window.');
     end
     
-    t = size(data,1);
-    est = upper(est);
+    vol = estimate_volatility_internal(data,est,bw,cln);
 
-    switch est
+end
+
+function vol = estimate_volatility_internal(data,est,bw,cln)
+
+    switch (est)
         case 'CC'
-            res = data.Returns;
-            para = sqrt(252);
-            fun = @(x) para * std(x);
+            res = data.Return .^ 2;
+            param = sqrt(252 / (bw - 1));
+            fun = @(x) param * sqrt(sum(x));
         case 'CCD'
-            res = (data.Returns - mean(data.Returns)) .^ 2;
-            para = 252 / (bw - 2);
-            fun = @(x) sqrt(para * sum(x));
+            res = (data.Return - nanmean(data.Return)) .^ 2;
+            param = sqrt(252 / (bw - 1));
+            fun = @(x) param * sqrt(sum(x));
         case 'GK'
             log_hl = log(data.High ./ data.Low);
             log_co = log(data.Close ./ data.Open);
             res = 0.5 * (log_hl .^ 2) - ((2 * log(2)) - 1) * (log_co .^ 2);
-            para = 252 / bw;
-            fun = @(x) sqrt(para * sum(x));
-        case 'HT'
-            res = data.Returns;
-            bw1 = bw - 1;
-            t1 = t - 1;
-            para = sqrt((1 / (1 - (bw / (t1 - bw1)) + ((bw^2 - 1) / (3 * (t1 - bw1)^2)))) * 252);
-            fun = @(x) para * std(x);
-        case 'P'        
+            param = sqrt(252 / bw);
+            fun = @(x) param * sqrt(sum(x));
+        case 'GKYZ'
             log_hl = log(data.High ./ data.Low);
-            res = (1 / (4 * log(2))) * (log_hl .^ 2);
-            para = 252 / bw;
-            fun = @(x) sqrt(para * sum(x));
-        case 'RS'
+            log_co = log(data.Close ./ data.Open);
+            log_oc = log(data.Open ./ [NaN; data.Close(1:end-1)]) .^ 2;
+            res = log_oc + 0.5 * (log_hl .^ 2) - ((2 * log(2)) - 1) * (log_co .^ 2);
+            param = sqrt(252 / bw);
+            fun = @(x) param * sqrt(sum(x));
+        case 'HT'
+            res = data.Return;
+            dif = t - bw;
+            param = sqrt(252 / (1 - (bw / dif) + (((bw ^ 2) - 1) / (3 * (dif ^ 2)))));
+            fun = @(x) param * std(x);
+        case 'P'        
+            res = log(data.High ./ data.Low) .^ 2;
+            param = sqrt(252 / (bw * 4 * log(2)));
+            fun = @(x) param * sqrt(sum(x));
+        case 'RS'          
             log_ho = log(data.High ./ data.Open);
             log_lo = log(data.Low ./ data.Open);
             log_co = log(data.Close ./ data.Open);
             res = (log_ho .* (log_ho - log_co)) + (log_lo .* (log_lo - log_co));
-            para = 252 / bw;
-            fun = @(x) sqrt(para * sum(x));
-        otherwise
-            error(['Invalid estimator "' est '" specified.']);
+            param = sqrt(252 / bw);
+            fun = @(x) param * sqrt(sum(x)); 
+        case 'YZ'
+            log_ho = log(data.High ./ data.Open);
+            log_lo = log(data.Low ./ data.Open);
+            log_co = log(data.Close ./ data.Open);
+            res_oc = log(data.Open ./ [NaN; data.Close(1:end-1)]) .^ 2;
+            res_cc = data.Return .^ 2;
+            res_rs = (log_ho .* (log_ho - log_co)) + (log_lo .* (log_lo - log_co));
+            res = [res_oc res_cc res_rs];
+            k = 0.34 / (1.34 + ((bw + 1) / (bw - 1)));
+            param1 = sqrt(252 / (bw - 1));
+            param2 = [1 k (1 - k)];
+            fun = @(x) param1 * sqrt(sum(sum(x,1) .* param2));        
     end
 
     win = get_rolling_windows(res,bw);
